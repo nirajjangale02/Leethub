8d1489a9007ea35871227d8144404926c68ac291
nirajjangale02 <85101637+nirajjangale02@users.noreply.github.com>
2022-09-16 21:56:40 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (53.76%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool checkString(string s) {
-        // int count=0;
-        // int temp=0;
-        // if(count==0){
-        //     return 1;
-        // }
-        // for(int i=0;i<s.length();i++){
-        //     if(s[i]=='a' ){
-        //         count++;
-        //     }
-        // }
-        // for(int i=0;i<count;i++){
-        //     if(s[i]=='a'){
-        //         temp++;
-        //     }
-        // }
-        // if(temp!=count){
-        //     return 0;
-        // }
-        // return 1;
-        for(int i=0;i<s.length()-1;i++){
-            if(s[i]>s[i+1]){
+        int count=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='a' ){
+                count++;
+            }
+        }
+        if(count==0){
+            return 1;
+        }
+        for(int i=0;i<count;i++){
+            if(s[i]=='b'){
                 return 0;
             }
-        } return 1;
+        }
+        return 1;
+       
     }
 };
